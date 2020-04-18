03e017951509954d4c632b4d2f911252492b3189
Pooja <yadavpuja2@gmail.com>
2020-04-18 18:51:35 +0530

    Global Exception handler

--- DateApp.api/Startup.cs
+++ DateApp.api/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using DateApp.api.Helpers;
 
 namespace DateApp.api
 {
@@ -52,7 +56,21 @@ namespace DateApp.api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }            
+            }   
+            else
+            {
+                app.UseExceptionHandler(builder=>{
+                    builder.Run(async context=>{
+                        context.Response.StatusCode=(int)HttpStatusCode.InternalServerError;
+                        var error=context.Features.Get<IExceptionHandlerFeature>();
+                        if(error!=null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+            }         
 
             app.UseRouting();          
             
