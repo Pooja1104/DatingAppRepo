ae131c513cd24b001ffe0aa303dc4c077ceb163d
Pooja <yadavpuja2@gmail.com>
2020-04-21 19:27:38 +0530

    ngx-bootstrap to power bootstrao components

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 
 import {BrowserModule} from "@angular/platform-browser"
 import {FormsModule} from "@angular/forms"
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from "../app/app.component"
 
 import {NavComponent} from "../app/nav/nav.component"
@@ -11,11 +11,12 @@ import {RegisterComponent} from "../app/register/register.component"
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 @NgModule(
     {
     declarations:[AppComponent,NavComponent,HomeComponent,RegisterComponent],
     bootstrap:[AppComponent],
-    imports:[BrowserModule,FormsModule,HttpClientModule],
+    imports:[BrowserModule,FormsModule,HttpClientModule,BsDropdownModule.forRoot(),BrowserAnimationsModule],
     providers:[
         AuthService,
         ErrorInterceptorProvider
